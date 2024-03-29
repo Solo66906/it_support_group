4badd84f39f8de2a8ecb45f047eeb533e2f9dbd5
Solo66906 <53848126+Solo66906@users.noreply.github.com>
2024-03-29 06:25:47 +0500

    All that's left is to make a slider

--- src/header/header-slider.js
+++ src/header/header-slider.js
@@ -1,9 +1,29 @@
 import React from "react";
+import ArrowRight from "../img/arrow_right.svg"
+
+const styles = {
+  p: {
+    fontWeight: 400,
+    fontSize: 60,
+    lineHeight: 1.06667,
+    width:600,
+  },
+  img: {
+    marginRight:15,
+  },
+}
 
 export default function HeaderSlider(){
   return (
     <div className="wrapperDesctop">
-      <p>Сантехника и мебель для ванной комнаты</p>
+      <p style={styles.p} className="white">Сантехника и мебель для ванной комнаты</p>
+      <ul className="slider">
+        <li><img style={styles.img} src={ArrowRight}/>О компании</li>
+        <li><hr className="line"/></li>
+        <li><img style={styles.img} src={ArrowRight}/>О продукции</li>
+        <li><hr className="line"/></li>
+        <li><img style={styles.img} src={ArrowRight}/>Каталог товаров</li>
+      </ul>
     </div>
   )
 }
